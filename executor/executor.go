f046c816b47315b577a55b1373f55e67b86d6636
zhangxuhai <lenshood.zxh@gmail.com>
2022-01-21 02:12:20 +0800

    feat: try dynamicly load wasm file (not test yet)

--- executor/executor.go
+++ executor/executor.go
@@ -1,8 +1,11 @@
 package executor
 
 import (
+	"encoding/base64"
 	"fmt"
 	. "github.com/TD-Hackathon-2022/DCoB-Scheduler/api"
+	"strings"
+	"syscall/js"
 )
 
 type Executor interface {
@@ -11,9 +14,62 @@ type Executor interface {
 	Interrupt() *Msg
 }
 
+const (
+	hashMiner = "hash-miner"
+	custom    = "custom-func-"
+)
+
 func ExecutorBuilder(msg *Msg) Executor {
-	if msg.GetAssign().FuncId != "hash-miner" {
-		fmt.Printf("Unsupported job types: %v", msg.GetAssign().FuncId)
+	funcId := msg.GetAssign().FuncId
+	if strings.HasPrefix(funcId, custom) {
+		funcPrefixArr := strings.Split(funcId, custom)
+		ch := loadCustomFunc(msg.GetAssign().Data, funcPrefixArr[1])
+		return newCustomFunc(ch)
 	}
-	return NewHashCoins(msg)
+
+	switch funcId {
+	case hashMiner:
+		return NewHashCoins(msg)
+	default:
+		fmt.Printf("Unsupported job types: %v", funcId)
+		return NewHashCoins(msg)
+	}
+}
+
+func loadCustomFunc(data string, prefix string) <-chan string {
+	content, _ := base64.StdEncoding.DecodeString(data)
+	prefixCh := make(chan string)
+	value := js.Global().Get("Uint8Array").New(len(content))
+	js.CopyBytesToJS(value, content)
+	g := js.Global().Get("Go").New()
+	js.Global().
+		Get("WebAssembly").
+		Call("instantiate", value, g.Get("importObject")).
+		Call("then", js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+			g.Call("run", args[0].Get("instance"))
+			prefixCh <- prefix
+			return nil
+		}))
+
+	return prefixCh
+}
+
+type customFunc struct {
+	prefix string
+}
+
+func newCustomFunc(prefix <-chan string) *customFunc {
+	return &customFunc{prefix: <-prefix}
+}
+
+func (h *customFunc) Start() {
+	js.Global().Call(h.prefix + "_start")
+}
+
+func (h *customFunc) Status() *Msg {
+	return nil
+}
+
+func (h *customFunc) Interrupt() *Msg {
+	return nil
 }
--- go.mod
+++ go.mod
@@ -2,18 +2,10 @@ module worker
 
 go 1.17
 
-replace (
-	github.com/TD-Hackathon-2022/DCoB-Scheduler => ../DCoB-Scheduler
-)
+replace github.com/TD-Hackathon-2022/DCoB-Scheduler => ../DCoB-Scheduler
 
 require (
+	github.com/TD-Hackathon-2022/DCoB-Scheduler v0.1.0
 	github.com/smartystreets/goconvey v1.7.2
 	google.golang.org/protobuf v1.27.1
-	github.com/TD-Hackathon-2022/DCoB-Scheduler v0.1.0
-)
-
-require (
-	github.com/gopherjs/gopherjs v0.0.0-20181017120253-0766667cb4d1 // indirect
-	github.com/jtolds/gls v4.20.0+incompatible // indirect
-	github.com/smartystreets/assertions v1.2.0 // indirect
 )
